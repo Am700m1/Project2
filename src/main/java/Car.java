de970704ee4ce686b0ed40ad0d7291a83064953b
Ammar <s202024140@kfupm.edu.sa>
2026-04-20 10:20:51 +0300

    Car class completion

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,3 +1,35 @@
 public class Car {
+    private String code;
+    private String route;
+    private int numberOfPassengers;
 
+    public Car(String code, String route, int numberOfPassengers){
+        this.code = code;
+        this.route = route;
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public String getRoute(){
+        return route;
+    }
+
+    public void setRoute(String route){
+        this.route = route;
+    }
+
+    public int getNumberOfPassengers(){
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
 }
